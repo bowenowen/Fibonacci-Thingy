ff7d3c6b3eff66e58c3ce30a758f3a7f326dbf02
owen chen <owenchen@owens-Mac.local>
2014-03-26 18:36:05 -0700

    Initial Commit

---
+++ Fibonacci ThingyTests/Fibonacci_ThingyTests.m
@@ -0,0 +1,34 @@
+//
+//  Fibonacci_ThingyTests.m
+//  Fibonacci ThingyTests
+//
+//  Created by owen chen on 3/26/2014.
+//  Copyright (c) 2014 bowen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Fibonacci_ThingyTests : XCTestCase
+
+@end
+
+@implementation Fibonacci_ThingyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
